dee877ded136b1cec50361dc574dabf94c3f047c
Zisisss123 <165705299+Zisisss123@users.noreply.github.com>
2024-04-01 22:41:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,27 @@ client.on('message', msg => {
     msg.reply('Pong!');
   } else {
     msg.reply(msg.content);
-  }
-});
+  client.on('voiceStateUpdate', (oldState, newState) => {
+  const { member } = newState;
+  const oldChannel = oldState.channel;
+  const newChannel = newState.channel;
+
+  if (!oldChannel && newChannel && newChannel.name === 'papi') {
+    // Member joined the "papi" voice channel
+    newChannel.guild.channels.create('podi', { type: 'voice' })
+      .then(createdChannel => {
+        member.voice.setChannel(createdChannel);
+      })
+      .catch(console.error);
+  } else if (oldChannel && oldChannel.name === 'papi' && (!newChannel || newChannel.name !== 'podi')) {
+    // Member left the "papi" voice channel or moved to a different channel
+    const podiChannel = oldChannel.guild.channels.cache.find(channel => channel.name === 'podi' && channel.type === 'voice');
+
+    if (podiChannel && podiChannel.members.size === 0) {
+      // If "podi" channel has 0 users, delete it
+      podiChannel.delete()
+        .then(deletedChannel => console.log(`Deleted channel ${deletedChannel.name}`))
+        .catch(console.error);
 
 client.on('guildMemberUpdate', (oldMember, newMember) => {
   // Extract necessary information
