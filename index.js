df9ff8b836b24e55a59fb2aef64fbaa6af5bf540
Zisisss123 <165705299+Zisisss123@users.noreply.github.com>
2024-04-02 21:49:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
+
 const Discord = require('discord.js');
 const keep_alive = require('./keep_alive.js');
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('guildMemberUpdate', (oldMember, newMember) => {
-  const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
-
-  if (addedRoles.size > 0) {
-    const guild = newMember.guild;
-    const randomChannel = guild.channels.cache.filter(channel => channel.type === 'text').random();
+client.on('message', async message => {
+  // Check if the message was sent by a user and includes "sold"
+  if (message.author.bot) return; // Ignore messages from bots
+  if (message.content.toLowerCase().includes('sold')) {
+    // Create an embed message with buttons attached
+    const embed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Items Sold')
+      .setDescription('Choose one of the following options:')
+      .addFields(
+        { name: '1', value: 'hahaha' },
+        { name: '2', value: 'suauac' }
+      );
     
-    if (randomChannel) {
-      const options = new Discord.MessageActionRow()
+    // Send the message with buttons attached
+    const sentMessage = await message.channel.send({ embeds: [embed], components: [
+      new Discord.MessageActionRow()
         .addComponents(
           new Discord.MessageButton()
-            .setCustomId('option1')
-            .setLabel('Hdhshd')
+            .setCustomId('option_1')
+            .setLabel('1')
             .setStyle('PRIMARY'),
           new Discord.MessageButton()
-            .setCustomId('option2')
-            .setLabel('ahdbuaon')
+            .setCustomId('option_2')
+            .setLabel('2')
             .setStyle('PRIMARY')
-        );
+        )
+    ]});
+    
+    // Add button click event listener
+    const filter = (interaction) => interaction.user.id === message.author.id;
+    const collector = sentMessage.createMessageComponentCollector({ filter, time: 15000 });
 
-      const embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Choose an option')
-        .setDescription('Please select one of the options below:')
-        .setFooter('Powered by ChatGPT');
-
-      randomChannel.send({ content: 'I am the one', embeds: [embed], components: [options] });
-    }
-  }
-});
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isButton()) return;
-
-  if (interaction.customId === 'option1') {
-    await interaction.reply({ content: 'You chose option 1.', ephemeral: true });
-  } else if (interaction.customId === 'option2') {
-    await interaction.reply({ content: 'You chose option 2.', ephemeral: true });
+    collector.on('collect', async interaction => {
+      if (interaction.customId === 'option_1') {
+        await interaction.reply({ content: 'You chose hahaha', ephemeral: true });
+      } else if (interaction.customId === 'option_2') {
+        await interaction.reply({ content: 'You chose suauac', ephemeral: true });
+      }
+    });
+    
+    collector.on('end', collected => {
+      console.log(`Collected ${collected.size} interactions.`);
+    });
   }
 });
 
